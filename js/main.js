daf2cda4a443c5e3d1d9643fef41ba4209ba2152
deicidem <kronosdidi@gmail.com>
2019-02-07 21:11:31 +0300

    a

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ $('.header-menu__button').on('click', () => {
   $('.header-menu__button').toggleClass('cross');
   $('.header-menu__nav').toggleClass('header-menu__nav_active');
 });
+$('.works-slider').slick({
+  mobileFirst: true,
+  slidesToShow: 1,
+  nextArrow: '<button type="button" class="works-next"></button>',
+  prevArrow: '<button type="button" class="works-prev"></button>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      }
+    }
+  ]
+})
